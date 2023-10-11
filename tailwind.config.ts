067814895ed43d1c5f27927576fb73909132b2eb
Vladislav Li <vladislav.li911@gmail.com>
2023-10-11 23:31:25 +0900

    created project

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import { nextui } from "@nextui-org/react";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  plugins: [nextui()],
+  darkMode: "class",
+};
+export default config;
